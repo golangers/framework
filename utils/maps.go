c01d85ab15cb170336d8a88d174de4198a51f477
leetaifook <lee@leetaifook.com>
2012-10-30 22:50:06 +0800

    Fixed maptosturct

--- utils/maps.go
+++ utils/maps.go
@@ -14,28 +14,44 @@ func (m M) MapToStruct(s interface{}) {
 	for i := 0; i < v.NumField(); i++ {
 		f := v.Type().Field(i)
 		key := f.Name
+		scnKey := Strings(key).SnakeCasedName()
 		tag := f.Tag
 		fieldName := tag.Get("field")
 		vf := v.Field(i)
-		if _, ok := m[key]; ok {
-			vf.Set(reflect.ValueOf(m[key]))
-		} else if fieldName != "" {
-			vv := reflect.ValueOf(m[fieldName])
-			if vf.Type().Kind().String() != vv.Type().Kind().String() {
-				if vf.Type().Kind().String() == "bool" {
-					if vv.Type().Kind().String() == "int64" && vv.Int() > 0 {
-						vf.SetBool(true)
-					}
-
-					if vv.Type().Kind().String() == "string" && vv.String() != "" {
-						ii, _ := strconv.ParseInt(vv.String(), 10, 64)
-						if ii > 0 {
+		doRes := false
+		if fieldName != "" {
+			if val, ok := m[fieldName]; ok {
+				vv := reflect.ValueOf(val)
+				if vf.Type().Kind().String() != vv.Type().Kind().String() {
+					if vf.Type().Kind().String() == "bool" {
+						if vv.Type().Kind().String() == "int64" && vv.Int() > 0 {
 							vf.SetBool(true)
 						}
+
+						if vv.Type().Kind().String() == "string" && vv.String() != "" {
+							ii, _ := strconv.ParseInt(vv.String(), 10, 64)
+							if ii > 0 {
+								vf.SetBool(true)
+							}
+						}
 					}
+				} else {
+					vf.Set(vv)
 				}
-			} else {
-				vf.Set(vv)
+
+				doRes = true
+			}
+		}
+
+		if !doRes {
+			if _, ok := m[key]; ok {
+				vf.Set(reflect.ValueOf(m[key]))
+			}
+		}
+
+		if !doRes {
+			if _, ok := m[scnKey]; ok {
+				vf.Set(reflect.ValueOf(m[scnKey]))
 			}
 		}
 	}
