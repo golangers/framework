4fe564a8f01febbb5432ff4c90229cb9efcb6d39
leetaifook <lee@leetaifook.com>
2012-11-21 12:05:27 +0800

    Update gob pre Reg UserType

--- cookiesession/cookiesession.go
+++ cookiesession/cookiesession.go
@@ -30,6 +30,10 @@ func init() {
 	gob.Register(map[int]interface{}{})
 	gob.Register(map[string]interface{}{})
 	gob.Register(map[interface{}]interface{}{})
+	gob.Register(map[string]string{})
+	gob.Register(map[int]string{})
+	gob.Register(map[int]int{})
+	gob.Register(map[int]int64{})
 }
 
 func encodeGob(obj map[string]interface{}) (string, error) {
--- filesession/filesession.go
+++ filesession/filesession.go
@@ -46,6 +46,10 @@ func init() {
 	gob.Register(map[int]interface{}{})
 	gob.Register(map[string]interface{}{})
 	gob.Register(map[interface{}]interface{}{})
+	gob.Register(map[string]string{})
+	gob.Register(map[int]string{})
+	gob.Register(map[int]int{})
+	gob.Register(map[int]int64{})
 }
 
 func encodeGob(obj map[string]interface{}) ([]byte, error) {
